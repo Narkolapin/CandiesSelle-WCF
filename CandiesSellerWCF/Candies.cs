7c8f2893a3d6b8d986dc05b6e9d526bcd4e3d43f
narkolapin <narkolapin@gmail.com>
2014-05-15 09:53:13 +0200

    Version 0.1 : Fonction getStock (affiche le stock)

--- CandiesSellerWCF/Candies.cs
+++ CandiesSellerWCF/Candies.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CandiesSellerWCF.Business;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -10,6 +11,10 @@ namespace CandiesSellerWCF
     // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" à la fois dans le code et le fichier de configuration.
     public class Candies : ICandies
     {
-
+        public stockBusiness getStock()
+        {
+            controlleurBusiness monControlleur = new controlleurBusiness();
+            return monControlleur.getStock();
+        }
     }
 }
--- CandiesSellerWCF/ICandies.cs
+++ CandiesSellerWCF/ICandies.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CandiesSellerWCF.Business;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -11,8 +12,8 @@ namespace CandiesSellerWCF
     [ServiceContract]
     public interface ICandies
     {
-        //[OperationContract]
-
+        [OperationContract]
+        stockBusiness getStock();
         
     }
 }
